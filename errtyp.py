b7df8ae7c9234c24e8880c2fe4b3437cccc2a79c
HANAMANTA <111106091+HANAMANTA041@users.noreply.github.com>
2022-08-16 09:43:18 +0530

    errtyp.md

---
+++ errtyp.py
@@ -0,0 +1,31 @@
+# IndexError
+# l = [1, 2, 3, 4]
+#print(l[5])
+# SyntaxError
+#print"hello"
+# ModularNotFoundError
+#import notamodule
+#KeyError
+#d = {'1': "hbs", '5': "car"}
+#print(d[5])
+#from math import cube # ImportError
+#it = iter([1, 2, 3]) # StopIterationError
+#print(next(it))
+#print(next(it))
+#print(next(it))
+#print(next(it))
+#a = "12"
+#b = 12
+#c = a+b
+#print("c") #TypeError
+#a = int('xyz') #ValueError
+#a = age #NameError
+#x=100/0
+#print("x") # Zerodivisionerror
+#name = input('enter your name')
+#print("name") #KeyboardInterruptError
+#if 5>3:
+#    print("123")
+
+#if 5<3:
+#print("123") #indentationerror
